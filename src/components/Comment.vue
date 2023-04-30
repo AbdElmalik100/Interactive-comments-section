934f9270d3997438cfab4d2b1048fca4657d73c9
Abd Elmalik Abd Elghafar <paydaychains100@gmail.com>
2023-05-01 01:03:55 +0300

    last-fixes

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -45,7 +45,7 @@
                         </span>
                     </div>
                     <div class="reply" v-else @click="confirmReply(comment, index + 1)">
-                        <img src="../assets/images/icon-reply.svg" alt="">
+                        <img src="../../public/images/icon-reply.svg" alt="">
                         <span class="ms-2 fw-bold">Reply</span>
                     </div>
                 </div>
